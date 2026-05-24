1ef8f4c32310c9ed11e3bb4c2f0b9e7eb7fe4cd5
AAAA9731 <alonprc@gmail.com>
2026-05-24 21:57:22 +0800

    add danger

---
+++ Patch/Patch_NightController_SummonerDefeated.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using nel;
+using XX;
+
+namespace WeNeedMoreNoels.Patch
+{
+    [HarmonyPatch(typeof(NightController), nameof(NightController.SummonerDefeated))]
+    public class Patch_NightController_SummonerDefeated
+    {
+        [HarmonyPrefix]
+        static void Prefix(ref int ob_add)
+        {
+            switch (WNMNTools.SyncType)
+            {
+                case EnemySyncType.StarterOnly:
+                    break;
+                case EnemySyncType.SmartAverage:
+                    float factor = 1 + 0.25f * (WNMNTools.TotalBattleNoelCount - 1);
+                    ob_add = X.IntU(ob_add * factor);
+                    break;
+                case EnemySyncType.Independent:
+                    ob_add *= WNMNTools.TotalBattleNoelCount;
+                    break;
+            }
+        }
+    }
+}
