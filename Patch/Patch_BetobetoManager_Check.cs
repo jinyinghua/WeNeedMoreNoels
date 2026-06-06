cff1658397a656764419a3f3c901969c2fd8f853
AAAA9731 <alonprc@gmail.com>
2026-06-06 22:18:32 +0800

    stop torn

---
+++ Patch/Patch_BetobetoManager_Check.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using nel;
+
+namespace WeNeedMoreNoels.Patch
+{
+    [HarmonyPatch(typeof(BetobetoManager), nameof(BetobetoManager.Check), [typeof(PR), typeof(NelAttackInfo), typeof(bool), typeof(bool)])]
+    public class Patch_BetobetoManager_Check
+    {
+        [HarmonyPrefix]
+        static bool Prefix()
+        {
+            return !DB.IsMultiplayer;
+        }
+    }
+}
